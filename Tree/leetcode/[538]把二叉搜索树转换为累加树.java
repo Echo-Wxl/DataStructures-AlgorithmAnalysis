5748521ba302294a5a989e2fc73693f5051d039a
wangxinlei <wangxinlei@insightone.cn>
2021-04-19 16:35:16 +0800

    Tree-update

---
+++ Tree/leetcode/[538]把二叉搜索树转换为累加树.java
@@ -0,0 +1,95 @@
+//给出二叉 搜索 树的根节点，该树的节点值各不相同，请你将其转换为累加树（Greater Sum Tree），使每个节点 node 的新值等于原树中大于或等于
+// node.val 的值之和。 
+//
+// 提醒一下，二叉搜索树满足下列约束条件： 
+//
+// 
+// 节点的左子树仅包含键 小于 节点键的节点。 
+// 节点的右子树仅包含键 大于 节点键的节点。 
+// 左右子树也必须是二叉搜索树。 
+// 
+//
+// 注意：本题和 1038: https://leetcode-cn.com/problems/binary-search-tree-to-greater-s
+//um-tree/ 相同 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 输入：[4,1,6,0,2,5,7,null,null,null,3,null,null,null,8]
+//输出：[30,36,21,36,35,26,15,null,null,null,33,null,null,null,8]
+// 
+//
+// 示例 2： 
+//
+// 输入：root = [0,null,1]
+//输出：[1,null,1]
+// 
+//
+// 示例 3： 
+//
+// 输入：root = [1,0,2]
+//输出：[3,3,2]
+// 
+//
+// 示例 4： 
+//
+// 输入：root = [3,2,4,1]
+//输出：[7,9,4,10]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中的节点数介于 0 和 104 之间。 
+// 每个节点的值介于 -104 和 104 之间。 
+// 树中的所有值 互不相同 。 
+// 给定的树为二叉搜索树。 
+// 
+// Related Topics 树 深度优先搜索 二叉搜索树 递归 
+// 👍 509 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+/**
+ * 思路：
+ * 将原本的升序变为降序， 即先打印右子树，再中序，再左子树
+ */
+class Solution {
+    int sum = 0;
+    public TreeNode convertBST(TreeNode root) {
+        inOrder(root);
+        return root;
+    }
+
+    private void inOrder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inOrder(root.right);
+        sum += root.val;
+        root.val = sum;
+        inOrder(root.left);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ Tree/leetcode/[654]最大二叉树.java
@@ -0,0 +1,96 @@
+//给定一个不含重复元素的整数数组 nums 。一个以此数组直接递归构建的 最大二叉树 定义如下： 
+//
+// 
+// 二叉树的根是数组 nums 中的最大元素。 
+// 左子树是通过数组中 最大值左边部分 递归构造出的最大二叉树。 
+// 右子树是通过数组中 最大值右边部分 递归构造出的最大二叉树。 
+// 
+//
+// 返回有给定数组 nums 构建的 最大二叉树 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [3,2,1,6,0,5]
+//输出：[6,3,5,null,2,0,null,null,1]
+//解释：递归调用如下所示：
+//- [3,2,1,6,0,5] 中的最大值是 6 ，左边部分是 [3,2,1] ，右边部分是 [0,5] 。
+//    - [3,2,1] 中的最大值是 3 ，左边部分是 [] ，右边部分是 [2,1] 。
+//        - 空数组，无子节点。
+//        - [2,1] 中的最大值是 2 ，左边部分是 [] ，右边部分是 [1] 。
+//            - 空数组，无子节点。
+//            - 只有一个元素，所以子节点是一个值为 1 的节点。
+//    - [0,5] 中的最大值是 5 ，左边部分是 [0] ，右边部分是 [] 。
+//        - 只有一个元素，所以子节点是一个值为 0 的节点。
+//        - 空数组，无子节点。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [3,2,1]
+//输出：[3,null,2,null,1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 1000 
+// 0 <= nums[i] <= 1000 
+// nums 中的所有整数 互不相同 
+// 
+// Related Topics 树 
+// 👍 268 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+/**
+ * 思路：
+ * 1.遍历数组等到最大值maxVal，得到root
+ * 2.对maxValue左边和右边得数组递归调用，作为root得左右子树
+ */
+class Solution {
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        return preOrder(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode preOrder(int[] nums, int lo, int hi) {
+        if (lo > hi) {
+            return null;
+        }
+
+        int index = -1, maxVal = Integer.MIN_VALUE;
+        for (int i = lo; i <= hi; i++) {
+            if (nums[i] > maxVal) {
+                maxVal = nums[i];
+                index = i;
+            }
+        }
+
+        TreeNode root = new TreeNode(maxVal);
+        root.left = preOrder(nums, lo, index - 1);
+        root.right = preOrder(nums, index + 1, hi);
+        return root;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
